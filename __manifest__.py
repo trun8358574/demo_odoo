6cc640a90b9ba3792d4b79d7ca6115d4bcde10ce
trun8358574 <50815148+trun8358574@users.noreply.github.com>
2019-05-20 10:18:06 +0700

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+  'name': 'Demo Addon',
+  'summary': 'Demo',
+  
+  'depends': [
+    'base',
+    'account',
+  ],
+  
+  'data': [
+    'views/demo_views.xml
+  ],
+  
+  'installable': True
+  'auto_install': False
+  
+
+}
