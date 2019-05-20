509aa2ad29b04a36b973f2c81f6c75ce3185b80c
trun8358574 <50815148+trun8358574@users.noreply.github.com>
2019-05-20 11:30:36 +0700

    Update my_models.py

--- models/my_models.py
+++ models/my_models.py
@@ -15,22 +15,22 @@ class IssueTicket(models.Model)
   text = fields.Text(string='Content', required=True)
   date = fields.Datetime(string='Ticket Date', 
                          default=lambda self: datetime.datetime.now, readonly=True)
-  order_number = fields.Integer(string='Order Number', required=True)
+  order_name = fields.Integer(string='Order Name', required=True)
   order_id = fields.Integer(string='Order ID Number', compute='_compute_order_id', store=True)
   order_record = fields.Many2one(comodel='sale.order', string='Order', compute='_compute_order', store=True)
   issues = fields.One2many(comodel='issue.issue', 'ticket_id')
   
   
-  @api.depends('order_number')
+  @api.depends('order_name')
   def _compute_order_id(self):
-    order_number = self.order_number
-    order = self.env['sale.order'].search(domains=[('order_number', '=', order_number)], limit=1)
+    order_name = self.order_name
+    order = self.env['sale.order'].search(domains=[('name', '=', order_name)], limit=1)
     self.order_id = order.id
     
-  @api.depends('order_number') 
+  @api.depends('order_name') 
     def _compute_order(self):
-    order_number = self.order_number
-    order = self.env['sale.order'].search(domains=[('order_number', '=', order_number)], limit=1)
+    order_name = self.order_name
+    order = self.env['sale.order'].search(domains=[('name', '=', order_name)], limit=1)
     self.order_record = order
     
     
