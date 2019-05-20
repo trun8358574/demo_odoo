a33b8b7e36498f01cc23bcbf83691db0a6573f9a
trun8358574 <50815148+trun8358574@users.noreply.github.com>
2019-05-20 14:15:53 +0700

    Add files via upload

---
+++ models/my_models.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields
+
+import datetime
+
+
+class IssueTicket(models.Model):
+    _name = 'issue.ticket'
+    _description = 'Issue Ticket'
+    _order = 'name desc'
+
+    name = fields.Char(string='Ticket Name', required=True)
+    text = fields.Text(string='Content', required=True)
+    date = fields.Datetime(string='Ticket Date',
+                         default=lambda self: datetime.datetime.now, readonly=True)
+    order_name = fields.Integer(string='Order Name', required=True)
+    order_id = fields.Integer(string='Order ID Number', compute='_compute_order_id', store=True)
+    order_record = fields.Many2one(comodel='sale.order', string='Order', compute='_compute_order', store=True)
+    issues = fields.One2many('issue.issue', 'ticket_id')
+  
+    @api.depends('order_name')
+    def _compute_order_id(self):
+        order_name = self.order_name
+        order = self.env['sale.order'].search(domains=[('name', '=', order_name)], limit=1)
+        self.order_id = order.id
+    
+    @api.depends('order_name')
+    def _compute_order(self):
+        order_name = self.order_name
+        order = self.env['sale.order'].search(domains=[('name', '=', order_name)], limit=1)
+        self.order_record = order
+    
+    
+class Issues(models.Model):
+    _name = 'issue.issue'
+    
+    ticket_id = fields.Many2one('issue.ticket', required=True)
+    content = fields.Char(string='Content')
+    
+   
+ 
